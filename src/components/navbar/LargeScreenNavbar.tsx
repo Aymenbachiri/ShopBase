317271980bdb53cfe0e1c69fd6a4872f37e52fd1
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-08-17 08:41:08 +0200

    add logo

--- src/components/navbar/LargeScreenNavbar.tsx
+++ src/components/navbar/LargeScreenNavbar.tsx
@@ -6,13 +6,23 @@ import getNavLinks from "./NavLinks";
 import IsAuth from "@/lib/helpers/IsAuth";
 import ProfileMenu from "./ProfileMenu";
 import CartLink from "./CartLink";
+import MyImage from "../reusable_components/MyImage";
+import logo from "/public/assets/images/logo.jpg";
 
 export default async function LargeScreenNavbar() {
   const navLinks = await getNavLinks();
 
   return (
     <nav className="max-w-screen-xl min-h-[50px] flex flex-wrap items-center justify-between mx-auto z-10">
-      <span>Logo</span>
+      <Link href="/" passHref>
+        <MyImage
+          src={logo}
+          alt="logo الشعار"
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+      </Link>
       <ul className="hidden md:flex justify-between gap-x-8 items-center px-4">
         {navLinks.map((link) => (
           <li key={link.href}>
